b4e1f056002fa04385ecb4acdc87ce4045b41816
Tonomoshia <tonomoshia@gmail.com>
2018-03-20 21:13:04 -0400

    quiz 6-8 on 3-20-18

---
+++ JavaScript/6-8_another_type_of_loop.js
@@ -0,0 +1,5 @@
+Directions:
+Use the array's forEach() method to loop over the following array and add 100 to each of the values if the value is divisible by 3.
+
+var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4, 19, 300, 3775, 299, 36, 209, 148, 169, 299, 6, 109, 20, 58, 139, 59, 3, 1, 139];
+Remember that the "Test Run" button will display any logged content, so feel free to use console.log() to test your code.
