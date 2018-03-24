ff4ca1c8dba566310978377d41aacdfc774bb133
Tonomoshia <tonomoshia@gmail.com>
2018-03-24 10:52:20 -0400

    quiz 7-5 in progress

---
+++ JavaScript/7-5_facebook_friends.js
@@ -0,0 +1,39 @@
+// Directions:
+// Create an object called facebookProfile. The object should have 3 properties:
+
+// your name
+// the number of friends you have, and
+// an array of messages you've posted (as strings)
+// The object should also have 4 methods:
+
+// postMessage(message) - adds a new message string to the array
+// deleteMessage(index) - removes the message corresponding to the index provided
+// addFriend() - increases the friend count by 1
+// removeFriend() - decreases the friend count by 1
+
+/*
+ * Programming Quiz: Facebook Friends (7-5)
+ */
+
+// your code goes here
+var facebookProfile = {
+    name: "Elizabeth Reiher",
+    friends: 350,
+    messages: ["I love kittens in the morning.", "How long does it take to get to Jersey?", "Sweet Christmas!", "My mom is a rock star!"],
+    postMessage(message) {
+
+
+    }, 
+    deleteMessage(index) {
+
+    },
+    addFriend() {
+
+    }
+    removeFriend() {
+        for (var friends = 350; friends < 351; friends--){
+            return friends;
+        }
+    }
+
+}
