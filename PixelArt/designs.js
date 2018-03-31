fd1dfd9b04e550ff5205d1ecf1bc4b780f2b98c7
Tonomoshia <tonomoshia@gmail.com>
2018-03-31 17:04:18 -0400

    pixel art maker

--- PixelArt/designs.js
+++ PixelArt/designs.js
@@ -1,10 +1,21 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
-}
+$(document).ready(function () {
+    $('#sizePicker').submit(function makeGrid(grid) {
+    $('table tr').remove();
+    var rows = $('#inputHeight').val();
+    var columns = $('#inputWeight').val();
+    for (var i = 1; i <= rows; i++) { 
+        $('table').append("<tr></tr>");
+        for (var j = 1; j <= columns; j++) {
+            $('tr:last').append("<td></td>");
+            $('td').attr("class", 'cells');
+        }
+      }
+      grid.preventDefault();
+      $('.cells').click(function (event) { // Color cells on click event
+        var color = $('#colorPicker').val();
+        $(event.target).css('background-color', color); // Choose  color on a click event
+      });
+    }); 
+  });
+  
